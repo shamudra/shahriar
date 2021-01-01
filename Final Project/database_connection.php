34c951322c1c3627a601a543f7d04bbc87989d14
shamudra <43570197+shamudra@users.noreply.github.com>
2021-01-01 21:35:56 +0600

    final project addded

---
+++ Final Project/database_connection.php
@@ -0,0 +1,14 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    else {
+        echo "Connected successfully";
+    }
+?>
---
+++ Final Project/sidebar.php
@@ -0,0 +1,14 @@
+<div style="position: fixed; top: 90px; left: 0px; height: calc(100vh - 190px); width: 200px; background-color: #d0d0d0;">
+    <div style="padding: 10px;">
+        <span style="font-size: large;"><a href="#">Patients</a></span>
+    </div>
+    <div style="padding: 10px;">
+        <span style="font-size: large;"><a href="#">Doctors</a></span>
+    </div>
+    <div style="padding: 10px;">
+        <span style="font-size: large;"><a href="#">Appointments</a></span>
+    </div>
+    <div style="padding: 10px;">
+        <span style="font-size: large;"><a href="#">Queries</a></span>
+    </div>
+</div>
